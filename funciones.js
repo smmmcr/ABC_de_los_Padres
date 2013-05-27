4583ce3f5196566d76539223385775aeae826336
Servicios de mercadeo <ingenieria@movilmultimediasa.com>
2013-05-27 17:55:43 -0600

    cambioScroll27052013
    me faltaria o un setTimeout o una manera de inicializar despues de crear
    la lista

--- funciones.js
+++ funciones.js
@@ -1,15 +1,30 @@
 var contenidoInicial;
 var idtema;
-/*
-$(document).ready(function(){
-contenidoInicial=$("#contenidoGeneral").html();
-/*$('select').selectmenu({ nativeMenu: "false" });*/
-/*});*/
-
-$(document).on('pageinit', function(){
+var myScroll;
+var a = 0;
+$(document).on('pagecreate', function(){
 	$("#contenedorCarga").hide();
+	 $.mobile.pushStateEnabled = true;
+		$.mobile.defaultDialogTransition = 'none';
+		$.mobile.defaultPageTransition = 'none';
 	$.mobile.allowCrossDomainPages = true;
+	 $.fn.disableSelection = function() {
+        return this
+                 .attr('unselectable', 'on')
+                 .css('user-select', 'none')
+                 .on('selectstart', false);
+    };
 });
+function loadedscroll(headerinter,footerinter,wrapper,scroller) {
+	setHeight(headerinter,footerinter,wrapper);
+	myScroll = new iScroll(scroller, {desktopCompatibility:true});
+}
+function setHeight(headerinter,footerinter,wrapper) {
+	var headerH = document.getElementById(headerinter).offsetHeight,
+		footerH = document.getElementById(footerinter).offsetHeight,
+		wrapperH = window.innerHeight - headerH - footerH;
+		document.getElementById(wrapper).style.height = wrapperH + 'px';
+}
 $("#TemasSemanales").on('pageinit', function(){
 	uri="https://movilmultimediasa.com/abcMobil/post.php?info=1";
 			$.getJSON(uri + '?function=' + 'check' + '&callback=?', function (json_data) {
@@ -31,120 +46,42 @@ $("#TemasSemanales").on('pageinit', function(){
 				}
 			});
 			});
-			scrollActiv("wrapper2","scroller2","no-padding2");
+		
+			//scrollActiv("wrapper2","scroller2","no-padding2");
 });
-$("#listaDeNombreBB1").on('pageinit', function(){
-generoN(2,$("#listaDeNombreBB1 ul"),"wrapper6","scroller6","no-padding2");
+$("#listaDeNombreBB1").on('pagecreate', function(){
+ $.fn.disableSelection = function() {
+        return this
+                 .attr('unselectable', 'on')
+                 .css('user-select', 'none')
+                 .on('selectstart', false);
+    };
+$("#listaDeNombreBB1 ul").disableSelection();
+$("#listaDeNombreBB1 ul").html("");;
+$("#listaDeNombreBB1 ul").append('<li id="wr"></li><li id="headerinter1"></li>');		
+if(generoN(2,$("#listaDeNombreBB1 ul"),"footerinter1")){
+
+}
+$("#footerinter1").bind("pageload",function(){ loadedscroll('headerinter1','footerinter1','wr','scrolle1');
+
 
 });
-$("#listaDeNombreBB2").on('pageinit', function(){
-generoN(1,$("#listaDeNombreBB2 ul"),"wrapper3","scroller3","no-padding3");
 });
-$("#Descuentos").on('pageinit', function(){
+$("#listaDeNombreBB2").on('pagecreate', function(){
+generoN(1,$("#listaDeNombreBB2 ul"),"footerinter1");
+//loadedscroll();
+ $.fn.disableSelection = function() {
+        return this
+                 .attr('unselectable', 'on')
+                 .css('user-select', 'none')
+                 .on('selectstart', false);
+    };
+$("#listaDeNombreBB2 ul").disableSelection();
+loadedscroll('headerinter2','footerinter2','wrapper2','scroller2');
+});
+$("#Descuentos").on('pagecreate', function(){
 descuentos();
-			scrollActiv("wrapper1","scroller1","no-padding1");
 });
-	
-function nuevoContenido(id){
-	switch(id){
-		case 1:
-	/*	 meses=new Array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
-			for(i=1;i<=31;i++){
-				$("#dia").append('<option value="'+i+'">'+i+'</option>');
-			}
-			for(i=0;i<12;i++){
-				$("#mes").append('<option value="'+(i-1)+'">'+meses[i]+'</option>');
-			}
-			for(i=2004;i<2020;i++){
-				$("#anno").append('<option value="'+(i)+'">'+i+'</option>');
-			}		*/
-			$(".header").html("<img src='img/headerInfoSemanall.png' />");
-			$("#pagInicio").hide();
-			$("#TemasSemanales").hide();
-			$("#Descuentos").hide();
-			$("#juegos").hide();
-			$("#TemasSemanales").hide();
-			/*$("#dia").html();
-			$("#mes").html();
-			$("#anno").html();*/
-			$("#pagInfoSemanal").show();
-		break;
-		case 2:
-			$(".header").html("<img src='img/headerTemaSemanall.png' />");
-			$("#pagInicio").hide();
-			$("#pagInfoSemanal").hide();
-			$("#TemasSemanales").hide();
-			$("#Descuentos").hide();
-			$("#juegos").hide();
-			$("#TemasSemanales").show();
-			
-		break;
-		case 3:
-			$(".header").html("<img src='img/headerTemasEmbarazo.png' />");
-			$("#pagInicio").hide();
-			$("#pagInfoSemanal").hide();
-			$("#TemasSemanales").hide();
-			$("#Descuentos").hide();
-			$("#juegos").hide();
-			$("#TemasEmbarazo").show();
-			$("#listaAcTemasEmba").show();
-			$("#eventoEmba1").hide();
-		break;
-		case 4:
-			$(".header").html("<img src='img/headerDescuentos.png' />");
-			$("#pagInicio").hide();
-			$("#pagInfoSemanal").hide();
-			$("#TemasSemanales").hide();
-			$("#TemasEmbarazo").hide();
-			$("#juegos").hide();
-			$("#Descuentos").show();
-			
-		break;
-		case 5:
-			$(".header").html("<img src='img/headerJuegos.png' />");
-			$("#pagInicio").hide();
-			$("#pagInfoSemanal").hide();
-			$("#TemasSemanales").hide();
-			$("#TemasEmbarazo").hide();
-			$("#Descuentos").hide();
-			$("#juegos").show();
-			juegos();
-		break;
-	
-	}
-}
-function irEvento(id){
-switch(id){
-	case 1: 
-	$("#listaAcTemasEmba").hide();
-	$("#textoNacido").hide();
-	/*$("#dia").html();
-	$("#mes").html();
-	$("#anno").html();
-/*	 meses=new Array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
-		for(i=1;i<=31;i++){
-			$("#eventoEmba1 #dia").append('<option value="'+i+'">'+i+'</option>');
-		}			
-		for(i=0;i<12;i++){
-			$("#eventoEmba1 #mes").append('<option value="'+(i-1)+'">'+meses[i]+'</option>');
-		}
-		for(i=2004;i<2020;i++){
-			$("#eventoEmba1 #anno").append('<option value="'+(i)+'">'+i+'</option>');
-		}*/
-	$("#eventoEmba1").show();
-	break;
-	case 3:
-		$("#listaAcTemasEmba").hide();
-		$("#textoNacido").hide();
-		$("#eventoEmba1").hide();
-		$(".listaDeNombreBB").hide();
-		$("#eventoEmba3").show();
-		$("#ninno, #ninna").show();
-	break;
-			
-}
-
-}
 function calcular(){
  meses=new Array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
 	Dia=$("#eventoEmba1 #dia").val();
@@ -162,24 +99,24 @@ function calcular(){
 	$("#eventoEmba1 #resultadoPar").html(fechaNacimiento);
 	$("#eventoEmba1 #resultadoPar").show();	
 }
-function generoN(id,objetos,wrapper,scroller,pad){
+function generoN(id,objetos,footerinter){		
 uri="https://movilmultimediasa.com/abcMobil/post.php?gen="+id;
 			$.getJSON(uri + '?function=' + 'check' + '&callback=?', function (json_data) {
-
-				objetos.html("");
 				for(index in json_data){
 				objetos.append("<li>"+json_data[index].nombre+"</li>");		
 				}
-scrollActiv(wrapper,scroller,pad);				
-				
+				objetos.append('<li></li><li></li><li id="'+footerinter+'"></li>');
 			});
+			return true;
+
 }
 function descuentos(){
-			
+
 			uri="https://movilmultimediasa.com/abcMobil/post.php?des=1";
 			$.getJSON(uri + '?function=' + 'check' + '&callback=?', function (json_data) {
 				$(".listaDescuentos").html("");
 				cont=1;
+				$(".listaDescuentos").append('<li id="wrapper"></li><li id="headerinter"></li>');				
 				for(index in json_data){
 				color="colorNormal";
 				if((cont%2)!=0){
@@ -190,11 +127,21 @@ function descuentos(){
 				"<p>Aplica desde: "+json_data[index].desde+"</p>"+
 				"<p>Hasta: "+json_data[index].hasta+"</p>"+
 				"<p>Descripci&oacute;n: "+json_data[index].descripcion+"</p>"+
+				"</div></li>");	
+				$(".listaDescuentos").append("<li class='"+color+"'><div class='imgPromo'><img src='https://movilmultimediasa.com/abcMobil/imgDescuentos/"+json_data[index].img+"'/></div><div class='textoDesc'>"+
+				"<h3>"+json_data[index].tituloPromo+"</h3>"+
+				"<p>Aplica desde: "+json_data[index].desde+"</p>"+
+				"<p>Hasta: "+json_data[index].hasta+"</p>"+
+				"<p>Descripci&oacute;n: "+json_data[index].descripcion+"</p>"+
 				"</div></li>");					
 				cont++;
 				}
+				$(".listaDescuentos").append('<li></li><li></li><li id="footerinter"></li>');
+		loadedscroll('headerinter','footerinter','wrapper','scroller');
+		$('.listaDescuentos').disableSelection();
 			});
 $(".listaDeNombreBB").show();
+
 }
 function juegos(){
 uri="https://movilmultimediasa.com/abcMobil/post.php?des=1";
@@ -236,6 +183,7 @@ var comentarios1=$("#comentario").val();
 	});
 
 }
+/*
 function scrollActiv(div,clase,pad){
 	myScroll = new iScroll( div, {
 		scrollbarClass: clase,
@@ -243,37 +191,31 @@ function scrollActiv(div,clase,pad){
 		fadeScrollbar:false,
 		hideScrollbar:false,
 		checkDOMChanges: true,
+		desktopCompatibility:true,
 		onBeforeScrollStart: function (e) {
 			var target = e.target;
 			while (target.nodeType != 1) target = target.parentNode;
 			if (target.tagName != 'SELECT' && target.tagName != 'INPUT' && target.tagName != 'TEXTAREA')
 				e.preventDefault();
 			}
-		});
-			
+		});			
 		document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
 		/*Etiqueta ALT del bot—n que se encuentra en el campo de busqueda*/
-		$('#contenedorgeneral').find('.ui-input-search .ui-input-clear').attr('title','Eliminar entrada');
-		
+	//	$('#contenedorgeneral').find('.ui-input-search .ui-input-clear').attr('title','Eliminar entrada');
 		/*	ID del contenido de la p‡gina mobile. 
 			Forzar refresco en la altura del elemento contenedor para que el scroll se 
 			muestre y se adapte sin problemas si redimensionamos el navegador
 		*/
-			
-		$(window).resize(function(){
+	/*	$(window).resize(function(){
 				$('#'+pad).width($(window).width());
 				$('#'+pad).height($(window).height()-45);
 		});
-
 		$(window).resize();
-		
 		/*INI iScroll forzar refresco para que la barra se muestre correctamente*/
-		
-		$(window).load(function() {
+	/*	$(window).load(function() {
   			myScroll.refresh();
 		});
-
-}
+}*/
 function volver(){
 $(".header").html("<img src='img/hederPrincipal.png' />");
 $("#pagInfoSemanal").hide();
