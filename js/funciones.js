a8e308b0cc650562692ba5967d0c8a94257de654
Servicios de mercadeo <ingenieria@movilmultimediasa.com>
2013-12-04 15:10:59 -0600

    cambios con logo

--- js/funciones.js
+++ js/funciones.js
@@ -13,8 +13,6 @@ document.addEventListener("deviceready", onDeviceReady, false);
 $(document).on('mobileinit', function(){
 descuentos();
 sincronizar();
-/*alert($(window).width());
-alert($(window).height());*/
 $("#contenedorCarga").hide();
 $.mobile.pushStateEnabled = true;
 $.mobile.defaultDialogTransition = 'none';
